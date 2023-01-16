950f7f4bfd31b9e198a005c124812a259ca84dc0
tykervella <kervellatyler@gmail.com>
2023-01-15 17:34:25 -0800

    made code functional, adding it to all trigger with event listener and display in the correct section of the  webpage

--- Develop/script.js
+++ Develop/script.js
@@ -1,74 +1,80 @@
+function generatePassword() {
+  
+  
+  var passLength = parseInt(prompt("How Long Would you like your passcode?","Choose a number 8-128"));
+
+  if (Number.isNaN(passLength) === true) {
+    alert ("Please select a number!");
+    return "Your passcode will appear here :)";
+  } else if (passLength >= 128) {
+    alert ("Please select a smaller number!");
+    return "Your passcode will appear here :)";
+  } else if (passLength <= 7){
+    alert ("Please select a larger number!");
+    return "Your passcode will appear here :)";
+  }; 
+
+  var characterType = ""
+  var uppercase = confirm("Would you like to include uppercase letters?");
+
+    if (uppercase === true) {
+      characterType += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    } else {
+      characterType = characterType;
+    };
+
+  var lowercase = confirm("Would you like to include lowercase letters?"); 
+
+    if (lowercase === true){
+      characterType += "abcdefghijklmnopqrstuvwxyz"
+    } else {
+      characterType = characterType;
+    };
+
+  var numbs = confirm("Would you like to include numbers?");
+    
+    if (numbs === true){ 
+      characterType += "0123456789"
+    } else {
+      characterType = characterType;
+    };
+
+  var specialchar = confirm("Would you like to include special characters?");
+
+    if (specialchar === true) {
+      characterType += ' !"#$%&()*+-./:;<=>?[]_^`|~'
+    } else {
+      characterType = characterType;
+    };
 
 
-var passLength = parseInt(prompt("How Long Would you like your passcode?","8-64"));
+    var result = "";
+    var length = passLength;
+    var characters = characterType;
+    var charactersLength = characters.length;
 
-var characterType = ""
+    for (var i =0; i < length; i++){
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
 
-var uppercase = confirm("Would you like to include uppercase letters?");
-
-var lowercase = confirm("Would you like to include lowercase letters?"); 
-
-var numbs = confirm("Would you like to include numbers?");
-
-var specialchar = confirm("Would you like to include special characters?");
-
-if (uppercase === true) {
-  characterType += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-} else {
-  characterType = characterType;
-};
-
-if (lowercase === true){
-  characterType += "abcdefghijklmnopqrstuvwxyz"
-} else {
-  characterType = characterType;
-};
-
-if (numbs === true){ 
-  characterType += "0123456789"
-} else {
-  characterType = characterType;
-};
-
-if (specialchar === true) {
-  characterType += ' !"#$%&()*+-./:;<=>?[]_^`|~'
-} else {
-  characterType = characterType;
-};
-
-
-
-
-console.log(passLength,typeof(passLength));
-
-function generatePassword(length) {
-  var result = "";
-  var characters = characterType; 
-  var charactersLength = characters.length;
-
-  for (var i =0; i < length; i++){
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
 
   return result;
-};
 
-console.log(generatePassword(passLength));
-
-
+}
 
 
 // // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// }
+  passwordText.value = password;
+
+}
 
 // // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword) 
+generateBtn.addEventListener("click", writePassword);
